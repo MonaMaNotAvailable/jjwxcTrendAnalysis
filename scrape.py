2d9765a3f16d9d8a8147f7ae63c9c4aeb1fba8b8
MonaMaNotAvailable <zma4@wpi.edu>
2024-01-08 11:48:53 -0500

    add 01072024

--- scrape.py
+++ scrape.py
@@ -1,6 +1,8 @@
-# TODO: preserve the link to each novel
-# TODO: allow variable for csv file name
+# TODO: allow variable for column name (date)
 # TODO: automate this script for weekly scraping
+# TODO: handle rank changing for new novel that gets into the top 200 chart (row swap/insertion/drop)
+# TODO: replace 进度 from 连载 to 完结 & update 字数 if changed
+# TODO: (optional) preserve the link to each novel
 
 import requests
 import pandas as pd
@@ -20,9 +22,28 @@ html_content = response.text
 # Parse the table
 tables = pd.read_html(html_content)
 
-# Assuming the table you need is the first one
-# print(tables[1])
+# Identify the table
 table = tables[1]
 
-# Save the table to a CSV file
-table.to_csv('晋江总分榜12312023.csv', index=False, encoding='utf_8_sig') # gb18030
+# Load the table from a CSV file
+df = pd.read_csv("晋江总分榜.csv")
+
+# Get the number of columns
+numColumns = df.shape[1]
+print(f'The number of columns in the DataFrame is: {numColumns}')
+
+# Insert the new data, extract the 作品积分 only
+df[numColumns] = table[6]
+# print(df.iat[0, numColumns])
+# Change 作品积分 to the date scraping the data
+df.iat[0, numColumns] = "01072024"
+
+df.to_csv('晋江总分榜.csv', index=False, encoding='utf_8_sig') # gb18030
+    
+# # # Insert the column 7发表时间 before the column 4进度 (put unchanged to the left while variable to the right)
+# # column_to_move = df.iloc[:, 7]
+# # column_name = df.columns[7]
+# # df.drop(df.columns[7], axis=1, inplace=True)
+# # df.insert(4, column_name, column_to_move)
+# # df.to_csv('晋江总分榜.csv', index=False, encoding='utf_8_sig') # gb18030
+# # print(df)
