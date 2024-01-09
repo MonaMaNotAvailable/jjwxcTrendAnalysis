9d1325fb3965788bbdd9b099fbb44439e4198c8a
MonaMaNotAvailable <zma4@wpi.edu>
2024-01-09 12:59:57 -0500

    change from insert to merge

--- scrape.py
+++ scrape.py
@@ -1,7 +1,6 @@
-# TODO: !!!fix 01072024 new data column (total points incorrect due to unmatched rows)
+# TODO: backward compatibility: new version of 晋江总分榜 is missing the first row (used to be the column names) line28
 # TODO: allow variable for csv name (date), taking input date or extract from system time
 # TODO: automate this script for weekly scraping
-# TODO: handle rank changing for new novel that gets into the top 200 chart (row swap/insertion/drop)
 # TODO: (optional) preserve the link to each novel
 
 import requests
@@ -25,55 +24,102 @@ tables = pd.read_html(html_content)
 # Identify the table
 table = tables[1]
 
+# Keep 3 columns only
+table = table[[1, 2, 6]]
+
 # Load the table from a CSV file
-df = pd.read_csv("晋江总分榜.csv")
+df = pd.read_csv("晋江总分榜.csv", dtype=str)
 
-# Get the number of columns
-numColumns = df.shape[1]
-# print(f'The number of columns in the DataFrame is: {numColumns}')
+# print(table)
+# print(df)
 
-# Insert the new data, extract the 作品积分 only
-df[numColumns] = table[6]
-# print(df.iat[0, numColumns])
+# Prepare for merging
+table.rename(columns={1: '1', 2: '2'}, inplace=True)
+
+# Merge based on 作者 & 作品
+merged = table.merge(df[['1','2','6']], on=['1','2'], how='outer')
 # Change 作品积分 to the date scraping the data
-df.iat[0, numColumns] = "01072024"
+merged.iat[0, 2] = "01092024"
 
-df.to_csv('晋江总分榜.csv', index=False, encoding='utf_8_sig') # gb18030
+# Assign the first row to be the column names
+merged.columns = merged.iloc[0]
+
+# Drop the first row
+merged = merged.drop(merged.index[0])
+
+# Reset the index and add it as a new column
+merged = merged.reset_index(drop=False)
+
+# Rename the new column
+merged.rename(columns={'index': '序号'}, inplace=True)
+# print(merged)
+
+# Count the number of NAN
+total_nan = merged.isna().sum().sum()
+print(f'Total number of NaN values in the newly merged df is: {total_nan}')
+
+# Replace all NaN values with 'missing'
+merged = merged.fillna('missing')
+
+merged.to_csv('晋江总分榜.csv', index=False, encoding='utf_8_sig') # gb18030
 
 
 
-# *****Statistics***** #
 
-# Should I switch back to separate weekly csv due to how sensitive this ranking is? but a lot of repeated data exist
-# print(table[2])
-# print(df.iloc[:, 2])
 
-# Write out the stats
-with open('WeeklyStats/01072024.txt', 'w') as file:
+# **********Statistics********** #
+
+# Write out the stats to separate txt file
+with open('WeeklyStats/01092024.txt', 'w') as file:
+    old = df.loc[:, '2']
+    new = table['2']
     # Check if the rankings are the same
-    comparison = table[2] == df.iloc[:, 2]
+    comparison = old == new
 
     # Count the number of different rankings
     false_count = (~comparison).sum()
 
     print(f'Number of changed rankings are: {false_count}', file=file)
 
-    # # Iterate over the comparison results
-    # for i in range(len(comparison)):
-    #     if not comparison.iloc[i]:
-    #         # Print the index and the differing values when a False is encountered
-    #         print(f'Rank: {i}, last week: {table.iloc[i, 2]}, this week: {df.iloc[i, 2]}')
+    # Elements in new but not in old
+    unique_to_df1 = table[~new.isin(old)]['2']
 
-    # Elements in table[2] but not in df.iloc[:, 2]
-    unique_to_df1 = table[~table[2].isin(df.iloc[:, 2])][2]
-
-    # Elements in df.iloc[:, 2] but not in table[2]
-    unique_to_df2 = df.loc[~df.iloc[:, 2].isin(table[2]), df.columns[2]]
+    # # Elements in old but not in new
+    unique_to_df2 = df[~old.isin(new)]['2']
 
     # Print the results
-    print("Drop out of the ranking (in last week but not in this week):", file=file)
+    print("\nDrop out of the ranking (in last week but not in this week):", file=file)
     for item in unique_to_df1:
         print(item, file=file)
     print("New to the ranking (in this week but not in last week):", file=file)
     for item in unique_to_df2:
         print(item, file=file)
+
+    # # Iterate over the comparison results
+    # for i in range(len(comparison)):
+    #     if not comparison.iloc[i]:
+    #         # Print the index and the differing values when a False is encountered
+    #         print(f'Rank: {i}, last week: {old.iloc[i]}, this week: {new.iloc[i]}')
+
+    # Reset the index to get the rankings
+    df_old = old.reset_index().rename(columns={'index': 'old_rank', '2': 'name'})
+    df_new = new.reset_index().rename(columns={'index': 'new_rank', '2': 'name'})
+
+    # Merge the DataFrames on the name
+    rankMerged = df_old.merge(df_new, on='name')
+
+    # Calculate the change in rankings (old rank - new rank), positive change indicates an upward movement
+    rankMerged['rank_change'] = rankMerged['old_rank'] - rankMerged['new_rank']
+
+    sorted = rankMerged.sort_values(by='rank_change', ascending=False)
+
+    # print(sorted)
+    print("\nSignificant rank changes:", file=file)
+
+    for i in range(len(sorted)):
+        if abs(sorted.iloc[i,3]) > 2:
+            if sorted.iloc[i,3] > 0:
+                print(f'{sorted.iloc[i,1]} rank: +{sorted.iloc[i,3]}, this week: {sorted.iloc[i,2]}, last week: {sorted.iloc[i,0]}', file=file)
+            else:
+                print(f'{sorted.iloc[i,1]} rank: {sorted.iloc[i,3]}, this week: {sorted.iloc[i,2]}, last week: {sorted.iloc[i,0]}', file=file)
+                
